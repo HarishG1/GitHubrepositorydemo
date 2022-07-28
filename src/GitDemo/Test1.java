d52cabd1f08224ff7d9902ff5fe64112d5eaf1b4
Hp <Hp@DESKTOP-GHG7Q89>
2022-07-28 12:05:46 +0530

    First Commit...

---
+++ src/GitDemo/Test1.java
@@ -0,0 +1,20 @@
+package GitDemo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry= "Yes";
+		if(hungry=="Yes")
+		{
+			System.out.println("Eat Something");
+		}
+		else
+		{
+			System.out.println("Do not eat anything");
+		}
+
+	}
+
+}
